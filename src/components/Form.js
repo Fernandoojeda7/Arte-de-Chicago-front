f43a841b056a9f4e9850d969fef7512c85003e88
Fernando Ojeda <fernandoojeda7@gmail.com>
2023-08-26 09:19:26 -0300

    fix message

--- src/components/Form.js
+++ src/components/Form.js
@@ -14,7 +14,8 @@ function FormularioContacto() {
   };
 
   const [formValues, setFormValues] = useState(initialValues);
-
+  const [validated, setValidated] = useState(false);
+  const [enviado, setEnviado] = useState(false);
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormValues({ ...formValues, [name]: value });
@@ -22,13 +23,29 @@ function FormularioContacto() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(formValues);
-    await formulario(formValues);
-    setFormValues(initialValues);
+    e.stopPropagation();
+    const form = e.currentTarget;
+    setValidated(true);
+    if (form.checkValidity() === false) {
+      return;
+    }
+    const respuesta = await formulario(formValues);
+    if (respuesta.message) {
+      setFormValues(initialValues);
+      setEnviado(true);
+      setTimeout(() => {
+        setEnviado(false);
+      }, 2000);
+    }
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit} noValidate validated={validated}>
+      {enviado && (
+        <div className="alert alert-success" role="alert">
+          Mensaje enviado correctamente
+        </div>
+      )}
       <Row className="g-3">
         <Form.Group as={Col} xs="12" md="6">
           <Form.Label>Email</Form.Label>
@@ -39,6 +56,9 @@ function FormularioContacto() {
             type="email"
             placeholder="Correo Electrónico"
           />
+          <Form.Control.Feedback type="invalid">
+            Debes ingresar tu Email
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group as={Col} xs="12" md="6">
@@ -49,6 +69,9 @@ function FormularioContacto() {
             name="fullName"
             placeholder="Nombre Completo"
           />
+          <Form.Control.Feedback type="invalid">
+            Debes ingresar tu Nombre y Apellido
+          </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group as={Col} xs="12">
@@ -59,6 +82,9 @@ function FormularioContacto() {
             name="phone"
             placeholder="Teléfono"
           />
+          <Form.Control.Feedback type="invalid">
+            Debes ingresar tu teléfono
+          </Form.Control.Feedback>
         </Form.Group>
         <Form.Group as={Col} xs="12">
           <Form.Label>Mensaje</Form.Label>
@@ -67,7 +93,12 @@ function FormularioContacto() {
             onChange={handleInputChange}
             name="message"
             placeholder="Mensaje"
+            as="textarea"
+            rows={1}
           />
+          <Form.Control.Feedback type="invalid">
+            Debes ingresar tu mensaje
+          </Form.Control.Feedback>
         </Form.Group>
         <Col
           xs="12"
